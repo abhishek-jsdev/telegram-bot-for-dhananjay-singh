f66bb40f8b036737a1e64e30e36216a1b7194f6d
IT MANAGER <>
2023-06-28 20:10:50 +0530

    minor code changes

--- py/ex02.py
+++ py/ex02.py
@@ -3,8 +3,8 @@ import os
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Call the function to clear the terminal
-clear_terminal()
+# # Call the function to clear the terminal
+# clear_terminal()
 
 # import logging
 # logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',level=logging.WARNING)
@@ -16,6 +16,8 @@ from telethon.tl.types import MessageEntityTextUrl,MessageEntityUrl, PeerChannel
 api_id=20340026
 api_hash='d1c2010562443ded33c1f4fa64f16bc4'
 client = TelegramClient('anon',api_id,api_hash)
+affiliate_id='dualwarez-21'
+log_count=0
 
 channels_id = {'offerzone':1315464303,'amazon':1491489500}
 
@@ -25,6 +27,71 @@ import urllib.parse
 def expand_short_url(short_url):
     response = requests.head(short_url, allow_redirects=True)
     return response.url
+
+def is_amazon_link(url):
+    if 'https://www.amazon.in' in url:
+        return True
+    return False
+
+# from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+import urllib.parse
+
+import re
+def update_amazon_link(long_url):
+    # url_parts = urllib.parse.urlparse(long_url)
+    # query = dict(urllib.parse.parse_qs(url_parts.query))
+    
+    # amazon_url_params={'tag':[affiliate_id],'ref':'','red':'','ref_':''}
+
+    # query.update(amazon_url_params)
+    # url_parts[4] = urllib.parse.urlencode(query)
+    # new_url = urllib.parse.urlunparse(url_parts)
+
+    # print(new_url)
+
+    # # print(query_params.get('tag'))
+    # if query_params.get('tag'):
+    #     del query_params['tag']
+    #     query_params['tag'] = [affiliate_id]
+    # else:
+    #     query_params['tag'] = [affiliate_id]
+
+    # if query_params.get('ref'):
+    #     del query_params['ref']
+    # if query_params.get('ref_'):
+    #     del query_params['ref_']
+    # if query_params.get('red'):
+    #     del query_params['red']
+    #     # query_params.tag[0]=[affiliate_id]
+
+    # Update query parameters with new values
+    # import urllib.parse
+
+    # url = "http://stackoverflow.com/search?q=question"
+    # params = {'lang':'en','tag':'python'}
+
+    # url_parts = urllib.parse.urlparse(url)
+    # query = dict(urllib.parse.parse_qsl(url_parts.query))
+    # query.update(params)
+
+    # new_url = url_parts._replace(query=urllib.parse.urlencode(query)).geturl()
+
+    # print(query_params)
+    # print(long_url)
+    # print(updated_url)
+    # return updated_url
+    
+    # start=re.match('tag=',long_url)
+    # print(re.search('tag=', long_url))
+    print(long_url[long_url.rfind('tag=')+4:])
+    long_url = long_url+'&'
+    if re.search('tag=', long_url):
+        pattern = r'tag=(.*?)&'
+        replaced_string = re.sub(pattern, affiliate_id,long_url)
+    else:
+        replaced_string = long_url+'&tag='+affiliate_id    
+    print('re: ',replaced_string)
+    # return replaced_string
 
 # listen for new message
 @client.on(events.NewMessage(chats = [1315464303,1491489500,810184328],incoming=True))
@@ -41,32 +108,33 @@ async def new_message_handler(event):
         for entity in event.message.entities:
             message=event.message.text
             if isinstance(entity,MessageEntityUrl):
-                # get url
                 url = message[entity.offset:entity.offset+entity.length]
-
                 long_url = expand_short_url(url)
-                # print(long_url)
-                # replace url
-                # new_message=message.replace(url,'https://www.youtube.com/')
-                new_message=message.replace(url,long_url)
-                event.message.text=new_message
+                if is_amazon_link(long_url):
+                    update_amazon_link(long_url)
+                    # replace url
+                    # new_message=message.replace(url,'https://www.youtube.com/')
+                    new_message=message.replace(url,long_url)
+                    event.message.text=new_message
                 
-                # print(event.message.text)
+                    # print(event.message.text)
             if isinstance(entity,MessageEntityTextUrl):
                 # get url
                 # url = message[entity.offset:entity.offset+entity.length]
                 long_url = expand_short_url(entity.url)
-                # print(long_url)
-                # replace url
-                # new_message=message.replace(entity.url,'https://www.youtube.com/')
-                new_message=message.replace(entity.url,long_url)
-                event.message.text=new_message
-                # replace url
-                # entity.url=await unshorten_url(entity.url)
+                if is_amazon_link(long_url):
+                    update_amazon_link(long_url)
+                    # print(long_url)
+                    # replace url
+                    # new_message=message.replace(entity.url,'https://www.youtube.com/')
+                    new_message=message.replace(entity.url,long_url)
+                    event.message.text=new_message
+                    # replace url
+                    # entity.url=await unshorten_url(entity.url)
                 
-        print(event.message.text)
-        for entity in event.message.entities:
-            print(entity)
+        # print(event.message.text)
+        # for entity in event.message.entities:
+        #     print(entity)
 
 
 
@@ -94,6 +162,16 @@ async def new_message_handler(event):
     except:
         print('error occured')
         pass
+
+    # finally:
+    #     if log_count>10:
+    #         clear_terminal()
+    #         log_count=0
+    #     else:
+    #         log_count +=1
+
+async def send_message():
+    client
 
 client.start()
 client.run_until_disconnected()
