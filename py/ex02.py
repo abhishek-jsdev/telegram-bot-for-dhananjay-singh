ddc6864aec80826e46001277e154caf8172c410e
IT MANAGER <>
2023-06-28 18:57:50 +0530

    feat: get messages from specific channels & and also update accordingly

--- py/ex02.py
+++ py/ex02.py
@@ -1,9 +1,16 @@
+import os
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Call the function to clear the terminal
+clear_terminal()
+
 # import logging
 # logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',level=logging.WARNING)
 
-import urlexpander
 from telethon import TelegramClient, events, errors
-from telethon.tl.types import MessageEntityTextUrl, PeerChannel
+from telethon.tl.types import MessageEntityTextUrl,MessageEntityUrl, PeerChannel
 
 # Use your own values from my.telegram.org
 api_id=20340026
@@ -12,36 +19,83 @@ client = TelegramClient('anon',api_id,api_hash)
 
 channels_id = {'offerzone':1315464303,'amazon':1491489500}
 
-async def unshorten_url(url):
-    return await urlexpander.expand(url)
+import requests
+import urllib.parse
 
-async def new_entity_url(url):
-    long_url = unshorten_url(url)
-    print(long_url)
-    return url
+def expand_short_url(short_url):
+    response = requests.head(short_url, allow_redirects=True)
+    return response.url
 
 # listen for new message
-@client.on(events.NewMessage)
+@client.on(events.NewMessage(chats = [1315464303,1491489500,810184328],incoming=True))
 async def new_message_handler(event):
     try:
-        message = event.message.message
-        peer_id = event.message.peer_id
-        entities = event.message.entities
+        # chat = await event.get_chat()
+        # print('chat: ',chat)    
+        # message=event.message.text
+        # print('message: \n',message)   
+        # raw_message=event.message.raw_text
+        # print('raw message: \n',raw_message)  
+        # entities=event.message.entities
+        # print('entities: \n',entities)
+        for entity in event.message.entities:
+            message=event.message.text
+            if isinstance(entity,MessageEntityUrl):
+                # get url
+                url = message[entity.offset:entity.offset+entity.length]
 
-        if isinstance(peer_id,PeerChannel):
-            for entity in entities:
-                if isinstance(entity,MessageEntityTextUrl):
-                    # entity.url = await new_entity_url(entity.url)
-                    print()
+                long_url = expand_short_url(url)
+                # print(long_url)
+                # replace url
+                # new_message=message.replace(url,'https://www.youtube.com/')
+                new_message=message.replace(url,long_url)
+                event.message.text=new_message
+                
+                # print(event.message.text)
+            if isinstance(entity,MessageEntityTextUrl):
+                # get url
+                # url = message[entity.offset:entity.offset+entity.length]
+                long_url = expand_short_url(entity.url)
+                # print(long_url)
+                # replace url
+                # new_message=message.replace(entity.url,'https://www.youtube.com/')
+                new_message=message.replace(entity.url,long_url)
+                event.message.text=new_message
+                # replace url
+                # entity.url=await unshorten_url(entity.url)
+                
+        print(event.message.text)
+        for entity in event.message.entities:
+            print(entity)
 
-        with open("logs.txt", "a") as f:
-            dict={'event':event,'message':message,'entities':entities,'peer_id':peer_id}
-            print(dict, file=f)
-            print()
+
+
+        # message = event.message.message
+        # peer_id = event.message.peer_id
+        # entities = event.message.entities
+
+        # if isinstance(peer_id,PeerChannel):
+        #     for entity in entities:
+        #         if isinstance(entity,MessageEntityTextUrl):
+        #             # entity.url = await new_entity_url(entity.url)
+        #             print()
+
+        # with open("logs.txt", "a") as f:
+        #     # dict={'event':event,'message':message,'entities':entities,'peer_id':peer_id}
+        #     # print(dict, file=f)
+        #     print('event::',event, file=f)
+        #     print('message::',message, file=f)
+        #     print('raw text::',event.raw_text,file=f)
+        #     print('entities::',entities, file=f)
+        #     print('peer_id::',peer_id, file=f)
+        #     print('',file=f)
 
         # print(peer_id,message)
     except:
+        print('error occured')
         pass
 
 client.start()
 client.run_until_disconnected()
+
+# 23
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+// constants.ts
+export const CONSTANTS = {
+  phoneNumber: 'Your Phone Number',
+  api_id: 'your telegram api id',
+  api_hash: 'your telegram api hash',
+};
