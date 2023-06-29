b7c9dda38c977982b4be897bb34722d7cfe824ca
IT MANAGER <>
2023-06-29 18:49:30 +0530

    alpha version is ready

---
+++ py/.trash.py
@@ -0,0 +1,38 @@
+# replace url
+                    # entity.url=await unshorten_url(entity.url)
+                
+        # print(event.message.text)
+        # for entity in event.message.entities:
+        #     print(entity)
+
+
+
+        # message = event.message.message
+        # peer_id = event.message.peer_id
+        # entities = event.message.entities
+
+        # if isinstance(peer_id,PeerChannel):
+        #     for entity in entities:
+        #         if isinstance(entity,MessageEntityTextUrl):
+        #             # entity.url = await new_entity_url(entity.url)
+        #             print()
+
+        # with open("logs.txt", "a") as f:
+        #     # dict={'event':event,'message':message,'entities':entities,'peer_id':peer_id}
+        #     # print(dict, file=f)
+        #     print('event::',event, file=f)
+        #     print('message::',message, file=f)
+        #     print('raw text::',event.raw_text,file=f)
+        #     print('entities::',entities, file=f)
+        #     print('peer_id::',peer_id, file=f)
+        #     print('',file=f)
+
+
+# chat = await event.get_chat()
+        # print('chat: ',chat)    
+        # message=event.message.text
+        # print('message: \n',message)   
+        # raw_message=event.message.raw_text
+        # print('raw message: \n',raw_message)  
+        # entities=event.message.entities
+        # print('entities: \n',entities)
