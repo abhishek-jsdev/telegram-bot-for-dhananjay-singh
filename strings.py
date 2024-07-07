c39c4bd3daaa8253471833cf2048ca35d026dcda
Abhishek R Chaurasiya <43162530+theboyofdream@users.noreply.github.com>
2024-07-07 19:25:03 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,9 +1,9 @@
 # editable variables
-API_ID = '20340026'    #Enter your Telegram app API ID
-API_HASH = 'd1c2010562443ded33c1f4fa64f16bc4'  # Enter the created API Hash 
+API_ID = '2034xxxx'    #Enter your Telegram app API ID
+API_HASH = 'd1c2011562443ded33c1f4fa64xxxxxx'  # Enter the created API Hash 
 AMAZON_AFFILIATE_ID = 'dualwarez-21'
 
-FROM_CHATS = [1315464303, 1491489500, 810184328, 1714047949,1450755585]#,980741307]
+FROM_CHATS = [1315464303, 1491489500, 810184328, 1714047949,1450755585] # Can be obtained using telegram web
 TEST_CHATS = [-810184328,-980741307]
 # TO_CHATS = -980741307
 TO_CHATS = -810184328
